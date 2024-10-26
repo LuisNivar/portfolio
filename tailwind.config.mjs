3f5121ef2900cfa005b6d6fdebc42030e6770363
LuisNivar <luisjosenivarn@hotmail.com>
2024-10-25 23:09:26 -0400

    WIP Add Initial elements

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        border: "background ease infinite",
+      },
+      keyframes: {
+        background: {
+          "0%": { transform: "rotate(0deg) scale(10)" },
+          "100%": { transform: "rotate(-360deg) scale(10)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
