fa5d9d3f47df6658e290473f6b8a0795df6837b2
LuisNivar <luisjosenivarn@hotmail.com>
2024-11-04 21:07:26 -0400

    Add Language Selector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,39 @@
 import english from "./en.json";
 import spanish from "./es.json";
+import UsFlag from "./../flags/us.astro";
+import SpainFlag from "./../flags/spain.astro"
 
 
-enum LANGUAGES {
+export enum LANGUAGES {
     ENGLISH = "en",
     SPANISH = "es"
 }
+
+export const LOCALE: Record<
+	string,
+	{ code: string; name: string; flag: typeof SpainFlag }
+> = {
+	en: {
+		code: 'en',
+		name: 'English',
+		flag: UsFlag,
+	},
+	es: {
+		code: 'es',
+		name: 'Español',
+		flag: SpainFlag,
+	},
+};
 
 export const getI18N = ({currentLocale = LANGUAGES.ENGLISH} : 
     { currentLocale : string | undefined } ) => {
     if(currentLocale === LANGUAGES.ENGLISH) return english;
     if(currentLocale === LANGUAGES.SPANISH) return spanish;
     return english;
+}
+
+export function getLangFromUrl(url: URL) {
+	const [, lang] = url.pathname.split('/');
+	if (lang === LANGUAGES.SPANISH) return LANGUAGES.SPANISH;
+	return LANGUAGES.ENGLISH;
 }
